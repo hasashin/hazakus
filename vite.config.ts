c729c9f08a4a7396ff35a970546f1a9f362424d3
hasashin <dominik@hazak.us>
2026-05-25 21:40:52 +0200

    Upgraded toolchain

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import { reactRouter } from '@react-router/dev/vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), tailwindcss(), reactRouter()],
+  plugins: [tailwindcss(), reactRouter()],
+  resolve: {
+    tsconfigPaths: true,
+  }
 })
